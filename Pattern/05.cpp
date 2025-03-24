2d892299a80e43788839a212af6ec4f2c6cbd0d7
Sameeksha Patel <130570218+SameekshaPatel02@users.noreply.github.com>
2025-03-24 22:43:01 +0530

    Add files via upload

---
+++ Pattern/05.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int Num){
+    for (int i = 0; i < Num; i++)
+    {
+        for (int j = Num; j > i; j--)
+        {
+            cout << "*" ;
+        }
+        cout << endl;
+    }
+    
+}
+
+int main(){
+    int Num;
+    cout << "Enter value of number:";
+    cin >> Num;
+    pattern(Num);
+}
