4259cfd91323565a6c8d966b38d51734982f7085
Sameeksha Patel <130570218+SameekshaPatel02@users.noreply.github.com>
2025-03-26 20:45:47 +0530

    Add files via upload
    Input Format: N = 3
    Result: 
      *  
     *** 
    *****   
    Input Format: N = 6
    Result:
         *     
        ***    
       *****   
      *******  
     ********* 
    ***********

---
+++ Pattern/07.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int N){
+    for (int i = 0; i < N; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }
+        // For printing the stars in each row
+        for(int j=0;j< 2*i+1;j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+        for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }    
+
+        cout << endl;
+    }
+    
+}
+
+int main(){
+    int N;
+    cout << "Enter value of number:";
+    cin >> N;
+    pattern(N);
+}
+
